b203467d1c2511d7c02ff61fa82e340bb0dcc010
Bruce Baron <baron.br@gmail.com>
2020-01-11 15:08:00 -0500

    :tada: First commit

---
+++ palindromes.test.js
@@ -0,0 +1,6 @@
+const palindromes = require('./palindromes')
+describe('palindromes()', () => {
+    it('correctly indentifies one-word palindromes',() => {
+        expect(palindromes('madam')).toEqual(['madam']);
+    });
+});

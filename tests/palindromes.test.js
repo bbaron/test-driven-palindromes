cd9061251d641b1f474ea6bfc5554542db9645e6
Bruce Baron <baron.br@gmail.com>
2020-01-11 15:51:11 -0500

    :truck: Move tests into own folder

--- tests/palindromes.test.js
+++ tests/palindromes.test.js
@@ -1,4 +1,4 @@
-const palindromes = require('./palindromes')
+const palindromes = require('../palindromes')
 describe('palindromes()', () => {
     it('correctly indentifies one-word palindromes',() => {
         expect(palindromes('madam')).toEqual(['madam']);
